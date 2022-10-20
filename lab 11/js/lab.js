a2f583b31a35c1b3135d487c46644227fd82a8eb
truiz97 <98765389+truiz97@users.noreply.github.com>
2022-10-19 22:22:18 -0700

    lessgooo

---
+++ lab 11/js/lab.js
@@ -0,0 +1,11 @@
+document.querySelector('.box')
+.style.backgroundColor = "yellow";
+
+//let turnYellow = document.querySelectorAll ('box');
+//allBoxes.forEach(('.box') => {
+  //.box.style.backgroundColor = 'yellow';
+//});
+
+//let turnYellow = (box) => {
+  //box.style.backgroundcolor = "yellow";
+//}
